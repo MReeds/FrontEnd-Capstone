7459f737cf3b80561a2001c68105f940e0af181d
Matt Reeds <mattlreeds@gmail.com>
2020-03-16 15:16:25 -0500

    reorganizing of files and authentication funciton created

---
+++ reassurance/src/components/Capstone.js
@@ -0,0 +1,31 @@
+import React, { useState } from "react";
+import NavBar from "./nav/NavBar";
+import ApplicationViews from "./ApplicationViews";
+
+const Capstone = () => {
+    // isAuthenticated is a function name that gets the credentials from session storage so long as it isnt a null value
+    const isAuthenticated = () => sessionStorage.getItem("credentials") !== null;
+
+    const [hasUser, setHasUser] = useState(isAuthenticated());
+
+    const setUser = (user, id) => {
+        sessionStorage.setItem("credentials", JSON.stringify(user));
+        sessionStorage.setItem("id", id)
+        setHasUser(isAuthenticated());
+    }
+    // clearUser function clears sessionStorage and sets hasUser back to empty credentials/falsey value
+    const clearUser = () => {
+        sessionStorage.clear();
+        setHasUser(isAuthenticated());
+    }
+
+    return (
+        // This return statement passes the hasUser, clearUser and setUser down to the child components as props
+        <>
+        <NavBar hasUser={hasUser} clearUser={clearUser} />
+        <ApplicationViews hasUser={hasUser} setUser={setUser}/>
+        </>
+    )
+}
+
+export default Capstone
