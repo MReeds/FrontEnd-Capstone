aefa70ee18e0961be2dd78b423b13c7eea10c76a
Matt Reeds <mattlreeds@gmail.com>
2020-03-16 23:12:11 -0500

    Not quite working yet. Need to do some debugging

--- src/components/ApplicationViews.js
+++ src/components/ApplicationViews.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { Route, Redirect } from "react-router-dom";
 import Home from "./Home/Home";
+import Login from "./auth/Login";
 
 const ApplicationViews = props => {
     // passing props to AV and declaring variable names equal to the props that were passed down from Capstone.js
@@ -9,6 +10,12 @@ const ApplicationViews = props => {
 
     return (
         <React.Fragment>
+            <Route 
+            path="/login"
+            render={props => {
+                return <Login setUser={setUser} { ...props}/>;
+            }}
+            />
             <Route
             exact path="/"
             render={props => {
