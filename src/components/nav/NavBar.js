8608b1472c0d10346d9bf5172fd9456a54bac314
Matt Reeds <mattlreeds@gmail.com>
2020-03-16 22:07:10 -0500

    added navBar and links

---
+++ src/components/nav/NavBar.js
@@ -0,0 +1,33 @@
+import React from "react";
+import { withRouter } from "react-router-dom";
+import { Link } from "react-router-dom";
+
+const NavBar = props => {
+    // NavBar funciton calls the clearUser function which clears the sessionStorage credentials and pushes the user to the home screen
+    const handleLogout = () => {
+        props.clearUser();
+        props.history.push("/");
+    };
+
+    return (
+        <header>
+            <h1 className="siteTitle">Reassurance</h1>
+            <nav>
+                <ul className="navContainer">
+                    <li>
+                        <Link className="navLink" to="/">
+                            Home
+                        </Link>
+                    </li>
+                    <li>
+                        <Link className="navLink" to="/" onClick={handleLogout}>
+                            Logout
+                        </Link>
+                    </li>
+                </ul>
+            </nav>
+        </header>
+    )
+}
+
+export default withRouter(NavBar)
